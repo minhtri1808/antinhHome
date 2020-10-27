522faa6676722aa54ba9c07de2f7a572917f3991
MigTri <trinm1808@gmail.com>
2020-10-27 22:10:10 +0700

    Home

---
+++ src/scripts/main.ts
@@ -0,0 +1,27 @@
+import { getSVGs, Loading } from "./utilities/util";
+import { Fullpage, FullpageOptions } from "./libraries/Fullpage";
+import Axios from "axios";
+
+
+const menu = ()=>{
+	const menu = document.querySelector("#menuRes");
+	const bot = document.querySelector(".header__bottom");
+	const social = document.querySelector(".footer__Isocial");
+	if(menu){
+		menu.addEventListener("click",(e)=>{
+			e.preventDefault();
+			console.log(1);
+			bot.classList.toggle("active");
+			social.classList.toggle("active");
+		})
+	}
+}
+
+document.addEventListener("DOMContentLoaded", async () => {
+	getSVGs(".svg");
+	Loading();
+	menu();
+
+});
+
+
