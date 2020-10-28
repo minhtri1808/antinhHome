aea1dcd4fd421c16bd1e2777a0dd8e7f4ee27a08
MigTri <trinm1808@gmail.com>
2020-10-28 21:05:54 +0700

    commit 2

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,7 +1,8 @@
 import { getSVGs, Loading } from "./utilities/util";
 import { Fullpage, FullpageOptions } from "./libraries/Fullpage";
 import Axios from "axios";
-
+// import  Swiper  from "swiper";
+declare var Swiper:any;
 
 const menu = ()=>{
 	const menu = document.querySelector("#menuRes");
@@ -17,11 +18,19 @@ const menu = ()=>{
 	}
 }
 
+const initSwiperProject = ()=>{
+	const swiperProject= new Swiper(".swiper__wrapper .swiper-container",{
+		speed: 1000,
+		spaceBetween: 24,
+		slidesPerView:3
+	})
+}
+
 document.addEventListener("DOMContentLoaded", async () => {
 	getSVGs(".svg");
 	Loading();
 	menu();
-
+	initSwiperProject();
 });
 
 
